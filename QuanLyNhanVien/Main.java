7347044323d732e4e892401dd8cdce2f5433d3be
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-12 09:17:28 +0700

    fix bug

--- QuanLyNhanVien/Main.java
+++ QuanLyNhanVien/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static NhanVien[] lowNhanVien;
     public static int pick;
     public static double avgSalary;
-    public static double sum11;
+    public static double sum11 = 0;
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -44,26 +44,15 @@ public class Main {
                     break;
                 }
                 case 2: {
-                    double salaryFulltime = RealSalaryFulltime(nhanVien);
-                    System.out.println(salaryFulltime);
-                    break;
-                }
-                case 3: {
-                    double salaryParttime = RealSalaryParttime(nhanVien);
-                    System.out.println(salaryParttime);
-                    break;
-                }
-                case 4: {
                     NhanVien[] newNhanVien1 = addMember(nhanVien);
                     System.out.println(Arrays.toString(newNhanVien1));
                     break;
                 }
-                case 5: {
-                    NhanVien[] lowSalary10 = lowSalaryMember(nhanVien);
-                    System.out.println(Arrays.toString(lowSalary10));
+                case 3: {
+                    lowSalaryMember(nhanVien);
                     break;
                 }
-                case 6: {
+                case 4: {
                     double allSum = allSalaryParttime(nhanVien);
                     System.out.println(allSum);
                     break;
@@ -76,27 +65,35 @@ public class Main {
         double sum = 0;
         for (int i = 0; i < nhanVien.length; i++) {
             if (nhanVien[i] instanceof NhanVienParttime) {
-                sum += realSalary2;
+                sum += RealSalaryParttime((NhanVienParttime) nhanVien[i]);
             }
         }
         return sum;
     }
 
-    public static NhanVien[] lowSalaryMember(NhanVien[] nhanVien) {
+    public static void lowSalaryMember(NhanVien[] nhanVien) {
         int j = 0;
+        int count = 0;
         for (int i = 0; i < nhanVien.length; i++) {
             if (nhanVien[i] instanceof NhanVienFulltime) {
-                sum11 += RealSalaryFulltime(nhanVien);
+                count++;
+                sum11 += RealSalaryFulltime((NhanVienFulltime) nhanVien[i]);
             }
         }
         avgSalary = sum11 / nhanVien.length;
+        lowNhanVien = new NhanVien[count];
         for (int i = 0; i < nhanVien.length; i++) {
-            if (realSalary1 < avgSalary) {
-                lowNhanVien[j] = nhanVien[i];
+            if (nhanVien[i] instanceof NhanVienFulltime) {
+                lowNhanVien[j] = (NhanVienFulltime) nhanVien[i];
                 j++;
             }
         }
-        return lowNhanVien;
+
+        for (int i = 0; i < lowNhanVien.length; i++) {
+            if (RealSalaryFulltime((NhanVienFulltime) lowNhanVien[i]) < avgSalary) {
+                System.out.println(lowNhanVien[i]);
+            }
+        }
     }
 
     public static NhanVien[] addMember(NhanVien[] nhanVien) {
@@ -118,11 +115,9 @@ public class Main {
 
     public static void menu() {
         System.out.println("1: Hiển thị thông tin nhân viên");
-        System.out.println("2: Lương thực lĩnh nhân viên fulltime");
-        System.out.println("3: Lương thực lĩnh nhân viên parttime");
-        System.out.println("4: Thêm một nhân viên");
-        System.out.println("5: Liệt kê những nhân viên fulltime có mức lương thấp hơn trung bình");
-        System.out.println("6: Tổng lương phải trả cho nhân viên parttime");
+        System.out.println("2: Thêm một nhân viên");
+        System.out.println("3: Liệt kê những nhân viên fulltime có mức lương thấp hơn trung bình");
+        System.out.println("4: Tổng lương phải trả cho nhân viên parttime");
     }
 
 
@@ -168,23 +163,14 @@ public class Main {
         return new NhanVienParttime(id, name, yearOld, phoneNumber, email, numberOfWork);
     }
 
-    public static double RealSalaryFulltime(NhanVien[] nhanVien) {
-        for (NhanVien x : nhanVien) {
-            if (x instanceof NhanVienFulltime) {
-                realSalary1 = ((NhanVienFulltime) x).getHardSalary() - ((NhanVienFulltime) x).getFinedMoney() +
-                        ((NhanVienFulltime) x).getBonusMoney();
-            }
-        }
+    public static double RealSalaryFulltime(NhanVienFulltime nhanVienFulltime) {
+        realSalary1 = nhanVienFulltime.getHardSalary() - nhanVienFulltime.getFinedMoney() + nhanVienFulltime.getBonusMoney();
         return realSalary1;
     }
-    
 
-    public static double RealSalaryParttime(NhanVien[] nhanVien) {
-        for (NhanVien x : nhanVien) {
-            if (x instanceof NhanVienParttime) {
-                realSalary2 = ((NhanVienParttime) x).getNumberOfWork() * 100000;
-            }
-        }
+
+    public static double RealSalaryParttime(NhanVienParttime nhanVienParttime) {
+        realSalary2 = nhanVienParttime.getNumberOfWork() * 100000;
         return realSalary2;
     }
 }
